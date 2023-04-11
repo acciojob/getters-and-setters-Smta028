77fb8464e3ab5ac16351c400e11e1ef61a15aacd
Samta <108336676+Smta028@users.noreply.github.com>
2023-04-11 16:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+	get getName(){
+		return this.name;
+	}
+		set setName(){
+			return this.name;
+		}
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	super()
+	study(){
+		console.log(getName()+"is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	super()
+	teach(){
+		console.log(getName()+"is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
